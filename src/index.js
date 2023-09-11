77b6e0d52566f20f9ddc2a9f47401e262fc1982a
Mortimel Ojeda <ojedamorti@gmail.com>
2023-09-10 20:48:43 -0400

    Lab Done

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 
 window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards()
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -38,14 +39,38 @@ window.addEventListener('load', (event) => {
     `;
   });
 
-  // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+  let pairsClicked = document.getElementById("pairs-clicked")
+  let pairsGuessed = document.getElementById("pairs-guessed")
 
-  // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if (memoryGame.pickedCards.length < 2) {
+        card.classList.toggle("turned")
+        memoryGame.pickedCards.push(card)
+      }
+      if (memoryGame.pickedCards.length === 2) {
+        setTimeout(() => {
+          if ((memoryGame.pickedCards[0] !== undefined) && (memoryGame.pickedCards[1] !== undefined)) {
+            memoryGame.pairsClicked++
+            let card1 = memoryGame.pickedCards[0].getAttribute("data-card-name")
+            let card2 = memoryGame.pickedCards[1].getAttribute("data-card-name")
+
+            if (memoryGame.checkIfPair(card1,card2)) {
+              memoryGame.pickedCards[0].classList.toggle("blocked")
+              memoryGame.pickedCards[1].classList.toggle("blocked")
+              memoryGame.pairsGuessed++
+            }
+            else {
+              memoryGame.pickedCards[0].classList.toggle("turned")
+              memoryGame.pickedCards[1].classList.toggle("turned")
+            }
+            memoryGame.pickedCards = []
+            pairsClicked.innerHTML = memoryGame.pairsClicked/2
+            pairsGuessed.innerHTML = memoryGame.pairsGuessed/2
+          }
+        }, 900)
+      }
     });
   });
 });
